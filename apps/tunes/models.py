88806dded33b924acd3da283745d099acaec3ed1
nickdibari <ndibari@fordham.edu>
2018-08-03 23:00:02 -0400

    Add docstring to Emotion

--- apps/tunes/models.py
+++ apps/tunes/models.py
@@ -7,7 +7,38 @@ from base.models import BaseModel
 
 class Emotion(BaseModel):
     """
+    Represents an "emotion" in the context of our offered moods we allow users
+    to select from. `lower_bound` and `upper_bound` are the lowest and highest
+    points to select songs from for a given emotion. These values must be
+    between 0 <= value <= 1. Emotions MAY overlap with other emotion
+    boundaries.
 
+    Emotion.name will be the three character value we store in the database.
+    To get the full name of an emotion, use the `full_name` property on an
+    instance of Emotion.
+    ```
+    > happy_record = Emotion.objects.get(name=Emotion.HAPPY)
+    > happy_record.name
+    >>> 'HPY'
+    > happy_record.fullname
+    >>> 'Happy'
+    ```
+
+    To add another emotion to the list, you'll need three things:
+        1) Add constant to class
+        MOODY = 'MOD'
+
+        2) Add constant to choices
+        EMOTION_NAME_CHOICES = [
+            #...
+            (MOODY, 'Moody'),
+        ]
+
+        3) Generate migration to add option to database
+        ./manage.py makemigrations tunes.Emotion
+            -> Please change the auto generated name to something more
+            insightful. For example, the migration file for this example
+            could be xxxx_add_moody_option_to_Emotion.py
     """
     MELANCHOLY = 'MEL'
     CALM = 'CLM'
