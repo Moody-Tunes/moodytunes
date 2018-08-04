6275773287c89d79db4386fe0810835e7440538d
nickdibari <ndibari@fordham.edu>
2018-08-04 14:40:38 -0400

    Set defaults on creation of UserEmotion
    Use ForeignKey instead of ManyToMany

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -19,14 +19,20 @@ class UserEmotion(BaseModel):
     values are specified upon creation, the boundaries will be set to the
     defaults defined in the `Emotion` table.
     """
-    user = models.ManyToManyField(MoodyUser)
-    emotion = models.ManyToManyField('tunes.Emotion')
+    user = models.ForeignKey(MoodyUser, on_delete=models.CASCADE)
+    emotion = models.ForeignKey('tunes.Emotion', on_delete=models.CASCADE)
     lower_bound = models.FloatField()
     upper_bound = models.FloatField()
 
     def save(self, *args, **kwargs):
-        # TODO: Set `lower_bound` and `upper_bound` to `emotion` defaults
-        pass
+        # Set lower_bound and upper_bound to emotion defaults
+        if not self.lower_bound:
+            self.lower_bound = self.emotion.lower_bound
+
+        if not self.upper_bound:
+            self.upper_bound = self.emotion.upper_bound
+
+        super().save(*args, **kwargs)
 
     def update_emotion_boundaries(self, emotion_name, song_name):
         # TODO: Figure out what to do about updating the boundaries given a
@@ -40,9 +46,9 @@ class UserSongVote(BaseModel):
     song made the user feel the linked emotion. This table will be used to
     track what songs a user associates with a particular emotion.
     """
-    user = models.ManyToManyField(MoodyUser)
-    song = models.ManyToManyField('tunes.Song')
-    emotion = models.ManyToManyField('tunes.Emotion')
+    user = models.ForeignKey(MoodyUser, on_delete=models.CASCADE)
+    song = models.ForeignKey('tunes.Song', on_delete=models.CASCADE)
+    emotion = models.ForeignKey('tunes.Emotion', on_delete=models.CASCADE)
     vote = models.BooleanField()
 
     def save(self, *args, **kwargs):
