9a4163132ba21a253878f17ac164c020f0d2030f
nickdibari <ndibari@fordham.edu>
2018-08-08 20:52:56 -0400

    Add validator unit tests

---
+++ apps/base/tests/test_validators.py
@@ -0,0 +1,21 @@
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+
+from base.validators import validate_decimal_value
+
+
+class TestDecimalValidator(TestCase):
+
+    def test_valid_values(self):
+        try:
+            validate_decimal_value(.5)
+        except ValidationError:
+            self.fail('Raise ValidationError when we did not want to')
+
+    def test_invalid_greater_than_one(self):
+        with self.assertRaises(ValidationError):
+            validate_decimal_value(2)
+
+    def test_invalid_less_than_zero(self):
+        with self.assertRaises(ValidationError):
+            validate_decimal_value(-2)
