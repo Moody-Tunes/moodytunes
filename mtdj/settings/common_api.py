f4db5d74271c23aa6180c63f9cc9c1673eaf9260
nickdibari <ndibari@fordham.edu>
2018-08-11 10:40:54 -0400

    Update default categories to be valid codes

--- mtdj/settings/common_api.py
+++ mtdj/settings/common_api.py
@@ -6,7 +6,7 @@ SPOTIFY_API_URL = env.str('MTDJ_SPOTIFY_API_URL', default='https://api.spotify.c
 SPOTIFY_AUTH_URL = env.str('MTDJ_SPOTIFY_API_URL', default='https://accounts.spotify.com/api/token')
 SPOTIFY_CLIENT_ID = env.str('MTDJ_SPOTIFY_CLIENT_ID', default='__spotify_client_id_not_set__')
 SPOTIFY_SECRET_KEY = env.str('MTDJ_SPOTIFY_SECRET_KEY', default='__spotify_secret_key_not_set__')
-SPOTIFY_CATEGORIES = env.list('MTDJ_SPOTIFY_CATEGORIES', default=['hiphop', 'rock', 'alternative'])
+SPOTIFY_CATEGORIES = env.list('MTDJ_SPOTIFY_CATEGORIES', default=['hiphop', 'rock', 'chill'])
 SPOTIFY_MAX_SONGS_FROM_LIST = env.int('MTDJ_SPOTIFY_MAX_SONGS_FROM_LIST', default=10)
 SPOTIFY_MAX_SONGS_FROM_CATEGORY = env.int('MTDJ_SPOTIFY_MAX_SONGS_FROM_CATEGORY', default=20)
 SPOTIFY_AUTH_CACHE_KEY = env.str('MTDJ_SPOTIFY_AUTH_CACHE_KEY', default='mtdj:spotify-auth-token')
