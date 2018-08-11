f3870cd7d56bef82e9d8e8cda0c1291ff140184e
nickdibari <ndibari@fordham.edu>
2018-08-11 04:30:49 -0400

    Add base management command

---
+++ apps/base/management/commands/__init__.py
@@ -0,0 +1,21 @@
+import logging
+import uuid
+
+from django.core.management.base import BaseCommand
+
+logger = logging.getLogger('mtdj.base.management.commands')
+
+
+class MoodyBaseCommand(BaseCommand):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._unique_id = uuid.uuid4()  # Include in logs for audit trail
+
+    """Override Django core command class to add hooks for our own logic"""
+    def execute(self, *args, **options):
+        logger.info('Command {name} about to run with {options}'.format(
+            name=self.__class__.__name__,
+            options=options
+        ))
+
+        return super().execute(*args, **options)
