42e42783a23e5b174419d7bb5aadced30c44f505
nickdibari <ndibari@fordham.edu>
2018-08-11 05:16:16 -0400

    Add Spotify URLs to settings

--- mtdj/settings/common_api.py
+++ mtdj/settings/common_api.py
@@ -1,5 +1,8 @@
 # API Keys/Credentials get stored here
 from envparse import env
 
+# Spotify Definitions
+SPOTIFY_API_URL = 'https://api.spotify.com/v1'
+SPOTIFY_AUTH_URL = 'https://accounts.spotify.com/api/token'
 SPOTIFY_CLIENT_ID = env.str('MTDJ_SPOTIFY_CLIENT_ID', default='__spotify_client_id_not_set__')
 SPOTIFY_SECRET_KEY = env.str('MTDJ_SPOTIFY_SECRET_KEY', default='__spotify_secret_key_not_set__')
