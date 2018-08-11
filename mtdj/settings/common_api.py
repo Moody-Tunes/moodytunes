ad93e1d074e2ecb559f1995a8bc3f96e0b0d00c1
nickdibari <ndibari@fordham.edu>
2018-08-11 08:29:22 -0400

    Add Spotify categories to settings
    Make API urls configurable

--- mtdj/settings/common_api.py
+++ mtdj/settings/common_api.py
@@ -2,7 +2,8 @@
 from envparse import env
 
 # Spotify Definitions
-SPOTIFY_API_URL = 'https://api.spotify.com/v1'
-SPOTIFY_AUTH_URL = 'https://accounts.spotify.com/api/token'
+SPOTIFY_API_URL = env.str('MTDJ_SPOTIFY_API_URL', default='https://api.spotify.com/v1')
+SPOTIFY_AUTH_URL = env.str('MTDJ_SPOTIFY_API_URL', default='https://accounts.spotify.com/api/token')
 SPOTIFY_CLIENT_ID = env.str('MTDJ_SPOTIFY_CLIENT_ID', default='__spotify_client_id_not_set__')
 SPOTIFY_SECRET_KEY = env.str('MTDJ_SPOTIFY_SECRET_KEY', default='__spotify_secret_key_not_set__')
+SPOTIFY_CATEGORIES = env.list('MTDJ_SPOTIFY_CATEGORIES', default=['hiphop', 'rock', 'alternative'])
