029fac5e4a5d90a9eb58fc0d719e20199298e0de
nickdibari <ndibari@fordham.edu>
2018-10-28 13:06:35 -0400

    Refactor home page to a redirect view

--- apps/base/views.py
+++ apps/base/views.py
@@ -1,11 +1,9 @@
-from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.views import View
+from django.views.generic.base import RedirectView
 
 
-class HomePageView(View):
-    # TODO: See if you can subclass `RedirectView` instead for this
-    def get(self, request):
-        if request.user.is_authenticated:
-            return HttpResponseRedirect(reverse('accounts:profile'))
-        return HttpResponseRedirect(reverse('accounts:login'))
+class HomePageView(RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return reverse('accounts:profile')
+        return reverse('accounts:login')
