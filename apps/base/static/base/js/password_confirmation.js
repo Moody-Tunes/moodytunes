4e4f568179aec4202a077250c8bdceda1fa77518
nickdibari <ndibari@fordham.edu>
2018-11-03 22:11:26 -0400

    Add JS to validate matching confirm password

---
+++ apps/base/static/base/js/password_confirmation.js
@@ -0,0 +1,20 @@
+'use strict';
+
+(function IIFE(){
+    var form = document.getElementsByTagName('form')[0];  // There's only the one form on these pages
+    form.addEventListener('submit', validateMatchingPassword);
+
+    function validateMatchingPassword(evt) {
+        // Validate that `confirm_password` and `password_fields` match
+        // Expects there to be a hidden div with id `password-error` to display errors on
+        var errorMessage = document.getElementById('password-error');
+        var passwordField = document.getElementById('id_password');
+        var confirmPasswordField = document.getElementById('id_confirm_password');
+
+        if (passwordField.value && passwordField.value !== confirmPasswordField.value) {
+            evt.preventDefault();
+            errorMessage.hidden = false;
+        }
+
+    }
+})();
