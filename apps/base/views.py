2f40788f0d97c4f3360207e7f396555b4366e2e0
nickdibari <ndibari@fordham.edu>
2018-11-14 00:32:09 -0500

    Add LoginRequired view mixin
    For marking views as requiring loging of some kind

--- apps/base/views.py
+++ apps/base/views.py
@@ -1,9 +1,15 @@
 from django.urls import reverse
 from django.views.generic.base import RedirectView
 
+from rest_framework.authentication import SessionAuthentication
+
 
 class HomePageView(RedirectView):
     def get_redirect_url(self, *args, **kwargs):
         if self.request.user.is_authenticated:
             return reverse('accounts:profile')
         return reverse('accounts:login')
+
+
+class LoginRequiredMixin(object):
+    permission_class = SessionAuthentication
