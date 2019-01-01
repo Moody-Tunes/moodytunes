4de4d3a0e3b11c1cfc3b7e9234b2477c17deebea
nickdibari <ndibari@fordham.edu>
2019-01-01 01:22:26 -0500

    Fix issue with settings module when using tox

--- mtdj/settings/__init__.py
+++ mtdj/settings/__init__.py
@@ -9,6 +9,11 @@ env.read_envfile(env_file)
 
 ENV = env.str('ENV', default='dev')
 
+# Special case for running unit tests
+if 'test' in os.environ['DJANGO_SETTINGS_MODULE']:
+    ENV = 'test'
+
+
 # Add apps/ and libs/ directory to Python path
 parent_dir = os.path.dirname  # For better readability
 BASE_DIR = parent_dir(parent_dir(parent_dir(os.path.abspath(__file__))))
@@ -18,10 +23,13 @@ LIBS_DIR = '{}/{}'.format(BASE_DIR, 'libs')
 sys.path.append(APPS_DIR)
 sys.path.append(LIBS_DIR)
 
+
 if ENV == 'dev':
     try:
         from .local_personal import *
     except ImportError:
         from .local import *
+elif ENV == 'test':
+    from .test import *
 else:
     from .common import *
--- mtdj/settings/test.py
+++ mtdj/settings/test.py
@@ -2,8 +2,6 @@
 
 from .common import *
 
-# Don't write to the console during unit test
-LOGGING['root']['handlers'] = ['app_file', 'error_file']
 
 # We want to make it easy to create test users, so we'll remove the password
 # validators when running tests
