19e806c0de59d502fdc6b0b82c1ca07953334eaa
nickdibari <ndibari@fordham.edu>
2019-01-10 21:34:35 -0500

    Fix typo in loaddata call
    Use double brackets for conditionals

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,25 +7,25 @@ PROJECT_PYTHON_VERSION="Python 3.5.2"
 # Ensure python3 version is 3.5
 python_version=$(python3 -V)
 
-if [ ! $python_version = $PROJECT_PYTHON_VERSION ]
+if [[ ! $python_version = $PROJECT_PYTHON_VERSION ]]
 then
     echo "Installing $PROJECT_PYTHON_VERSION"
     sudo apt-get install python3.5 -y >/dev/null
 fi
 
 # Check if python3 venv is installed
-python3 -m venv venv > /dev/null
+python3.5 -m venv venv > /dev/null
 
-if [ $? -eq 1 ]
+if [[ $? -eq 1 ]]
 then
 	echo "Installing python3 virtualenv..."
 	sudo apt-get install python3.5-venv -y > /dev/null
 fi
 
-if [ ! -d venv ]
+if [[ ! -d venv ]]
 then
 	echo "Creating virtual environment..."
-	python3 -m venv venv
+	python3.5 -m venv venv
 fi
 
 echo "Activating python virtual environment..."
@@ -36,7 +36,7 @@ pip3 install -r requirements/dev.txt
 
 echo "Setting up Django project..."
 python manage.py migrate
-python loaddata apps/tunes/fixtures/Initial_Songs.json
+python manage.py loaddata apps/tunes/fixtures/Initial_Songs.json
 
 echo "Finished installing mtdj!"
 echo "In the future, execute 'source venv/bin/activate' to work in the created virtual environment"
