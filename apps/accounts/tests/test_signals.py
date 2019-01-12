47bd79cf938fb8923965e40f86f0fd8bcea6e3da
nickdibari <ndibari@fordham.edu>
2019-01-12 01:06:35 -0500

    Fix unit test

--- apps/accounts/tests/test_signals.py
+++ apps/accounts/tests/test_signals.py
@@ -10,15 +10,10 @@ class TestCreateUserEmotionRecordsSignal(TestCase):
     def test_post_save_user_creates_all_user_emotion_records(self):
         user = MoodyUtil.create_user(username='test_signal_user')
 
-        existing_emotions = Emotion.objects.all().order_by('-name')
-        created_user_emotions = user.useremotion_set.all().order_by(
-            '-emotion__name'
-            ).values_list(
-            'emotion__name',
-            flat=True
-        )
+        existing_emotions = Emotion.objects.all()
 
-        self.assertQuerysetEqual(existing_emotions, created_user_emotions)
+        for emotion in existing_emotions:
+            self.assertTrue(user.useremotion_set.filter(emotion__name=emotion.name).exists())
 
 
 class TestUpdateUserBoundariesSignal(TestCase):
