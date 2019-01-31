f2645a17fcb62999460b26b483ac101a44f4e22b
nickdibari <ndibari@fordham.edu>
2019-01-30 19:54:28 -0500

    First pass at options endpoint

--- apps/base/static/base/js/client.js
+++ apps/base/static/base/js/client.js
@@ -2,8 +2,34 @@
 
 (function IIFE() {
     document.MoodyTunesClient = {
-        sayHello: function () {
-            alert('Hello!');
+        buildRequestURL: function(url, params) {
+            var requestUrl = new URL(window.location.origin + url);
+            for (var key in params) {
+                if (params.hasOwnProperty(key)) {
+                    requestUrl.searchParams.append(key, params[key]);
+                }
+            }
+
+            return requestUrl;
+        },
+        options: function () {
+            var url = this.buildRequestURL('/tunes/options/', {});
+
+            fetch(url, {
+                method: 'GET',
+                credentials: 'include',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            }).then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error('Bad response from ' + url);
+                }
+            }).then((json) => {
+                console.log(json);
+            });
         }
     };
 })();
