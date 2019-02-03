d0f806c252846e8e164d4c25169baad279360596
nickdibari <ndibari@fordham.edu>
2019-02-02 23:19:04 -0500

    Add javascript for voting on songs

--- apps/moodytunes/static/moodytunes/js/browse.js
+++ apps/moodytunes/static/moodytunes/js/browse.js
@@ -1,4 +1,16 @@
 (function IIFE() {
+    function voteOnSong() {
+        var emotion = document.getElementById('id_emotion').value;
+        var song = this.dataset.song;
+        var vote = this.dataset.vote;
+
+        document.MoodyTunesClient.postVote(song, emotion, vote, function(data) {
+            // Hide container holding song to prevent double votes
+            var songContainer = document.getElementById('song-' + song);
+            songContainer.hidden = true;
+        })
+    }
+
     function displayBrowsePlayList(data) {
         var playlistContainer = document.getElementById('playlist-display-container');
 
@@ -13,12 +25,30 @@
         for (var i=0; i<data.length; i++) {
             var song = data[i];
             var listRecord = document.createElement('li');
+            var songContainer = document.createElement('div');
+            listRecord.id = 'song-' + song.code;
 
             // Generate Spotify play button for track and add to playlist
             var playButton = document.createElement('iframe');
             playButton.src = 'https://embed.spotify.com/?uri=' + song.code;
+            songContainer.appendChild(playButton);
 
-            listRecord.appendChild(playButton);
+            // Generate voting buttons
+            var upvoteButton = document.createElement('button');
+            upvoteButton.appendChild(document.createTextNode('Upvote'));
+            upvoteButton.dataset.song = song.code;
+            upvoteButton.dataset.vote = true;
+            upvoteButton.addEventListener('click', voteOnSong);
+            songContainer.appendChild(upvoteButton);
+
+            var downvoteButton = document.createElement('button');
+            downvoteButton.appendChild(document.createTextNode('Downvote'));
+            downvoteButton.dataset.song = song.code;
+            downvoteButton.dataset.vote = false;
+            downvoteButton.addEventListener('click', voteOnSong);
+            songContainer.appendChild(downvoteButton);
+
+            listRecord.appendChild(songContainer);
             playButtonList.appendChild(listRecord);
         }
 
