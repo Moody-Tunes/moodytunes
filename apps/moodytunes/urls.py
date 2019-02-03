014d1fc2b5878304d2481eb8dd287da7049c1194
nickdibari <ndibari@fordham.edu>
2019-02-02 21:40:54 -0500

    Skeleton for browse page

--- apps/moodytunes/urls.py
+++ apps/moodytunes/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 
+from moodytunes import views
+
 urlpatterns = [
+    path('browse/', views.BrowsePlaylistsView.as_view(), name='browse')
 ]
--- apps/moodytunes/views.py
+++ apps/moodytunes/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+@method_decorator(login_required, name='dispatch')
+class BrowsePlaylistsView(TemplateView):
+    template_name = 'browse.html'
