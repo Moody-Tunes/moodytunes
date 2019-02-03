307f29443a10ba93874db5c6c7802b438e075f2f
nickdibari <ndibari@fordham.edu>
2019-02-02 22:24:51 -0500

    Add javascript to generate browse playlist

--- apps/base/static/base/js/client.js
+++ apps/base/static/base/js/client.js
@@ -116,7 +116,7 @@
             };
             this.request('GET', '/accounts/analytics/', params, {}, callback);
         },
-        getBrowsePlaylist: function(emotion, jitter, limit, genre, callback) {
+        getBrowsePlaylist: function(emotion, jitter, limit, genre, context, description, callback) {
             // Retrieve a playlist of songs for a user to listen to and decide whether or not they make them feel
             // their desired emotion
             var params = {
--- apps/moodytunes/static/moodytunes/js/browse.js
+++ apps/moodytunes/static/moodytunes/js/browse.js
@@ -1,8 +1,24 @@
 (function IIFE() {
-    function buildPlaylistOptions(options) {
-        // Build form for submitting request for playlist
-
+    function displayBrowsePlayList(data) {
+        alert('Made request OK!');
+        console.log('Data for playlist: ' + data);
     }
 
-    document.MoodyTunesClient.getOptions(buildPlaylistOptions);
+    function getBrowsePlaylist() {
+        // TODO: Set these values from user input
+        var jitter = undefined;
+        var limit = undefined;
+
+        var emotion = document.getElementById('id_emotion').value;
+        var genre = document.getElementById('id_genre').value || undefined;
+        var context = document.getElementById('id_context').value || undefined;
+        var description = document.getElementById('id_description').value || undefined;
+
+        document.MoodyTunesClient.getBrowsePlaylist(
+            emotion, jitter, limit, genre, context, description, displayBrowsePlayList
+        );
+    }
+
+    var generatePlaylistButton = document.getElementById('generate-playlist');
+    generatePlaylistButton.onclick = getBrowsePlaylist;
 })();
