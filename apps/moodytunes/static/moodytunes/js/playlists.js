5fea2dba2c47f70d851efc2730fbd34f0891db79
nickdibari <ndibari@fordham.edu>
2019-02-08 23:03:22 -0500

    Change color of song container when vote deleted

--- apps/moodytunes/static/moodytunes/js/playlists.js
+++ apps/moodytunes/static/moodytunes/js/playlists.js
@@ -6,6 +6,7 @@
         document.MoodyTunesClient.deleteVote(song, emotion, function(data) {
             // Disable buttons to prevent double votes for a track
             var songContainer = document.getElementById('song-' + song);
+            songContainer.className += ' song-container-vote-delete';
             var voteButtons = songContainer.querySelectorAll('button');
 
             for (var i=0; i<voteButtons.length; i++) {
