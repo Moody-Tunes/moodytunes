b804f4983ceac5f12db3668995da0ece613e9cb6
nickdibari <ndibari@fordham.edu>
2019-02-09 21:01:56 -0500

    Add periodic task record for task

--- mtdj/settings/common_celery.py
+++ mtdj/settings/common_celery.py
@@ -1,4 +1,6 @@
+from celery.schedules import crontab
 from envparse import env
+
 
 CELERY_TASK_ALWAYS_EAGER = env.bool('MTDJ_CELERY_TASK_ALWAYS_EAGER', default=False)
 CELERY_BROKER_URL = env.str('MTDJ_CELERY_BROKER_URL', default='__broker_not_set__')
@@ -6,10 +8,16 @@ CELERY_TASK_SERIALIZER = 'json'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_RESULT_BACKEND = 'django-db'
+CELERY_TIMEZONE = 'America/New_York'
 
 # Define periodic tasks here as key:value pairs
 # Key should be a string identifying the task to be run
 # Value should be a dictionary containing configurations for the periodic task
 #   - task: dotted.path.to.task
 #   - schedule: Scheduler to use for calling task (crontab, seconds value, etc.)
-CELERY_BEAT_SCHEDULE = {}
+CELERY_BEAT_SCHEDULE = {
+    'clear-expired-sessions': {
+        'task': 'base.tasks.clear_expired_sessions',
+        'schedule': crontab(hour=2, day_of_week=0)
+    }
+}
