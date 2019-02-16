db8c187e0cbf24dd05dd041cf0143cbf87c7ff59
nickdibari <ndibari@fordham.edu>
2019-02-15 19:45:24 -0500

    Add task to call command to generate songs from spotify

---
+++ apps/tunes/tasks.py
@@ -0,0 +1,18 @@
+import logging
+import os
+
+from celery.task import task
+from django.core.management import call_command
+
+logger = logging.getLogger(__name__)
+
+
+@task(bind=True)
+def create_songs_from_spotify_task(self):
+    """Periodic task to create songs from Spotify"""
+    logger.info('Calling command to create songs from Spotify')
+
+    # Disable writing to standard output when running command
+    # Everything that we write to stdout gets logged anyway
+    with open(os.devnull, 'w') as dev_null:
+        call_command('tunes_create_songs_from_spotify', stdout=dev_null, stderr=dev_null)
