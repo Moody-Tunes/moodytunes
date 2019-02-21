d8e656da63ad6ae20f6c058cd014317259bde991
nickdibari <ndibari@fordham.edu>
2019-02-20 22:33:56 -0500

    Add method to generate random unicode string

--- libs/tests/helpers.py
+++ libs/tests/helpers.py
@@ -5,6 +5,34 @@ from accounts.models import MoodyUser
 from tunes.models import Song, Emotion
 
 
+def generate_random_unicode_string(length):
+    # From https://stackoverflow.com/questions/1477294/generate-random-utf-8-string-in-python
+    # Credit: Jacob Wan
+
+    include_ranges = [
+        ( 0x0021, 0x0021 ),
+        ( 0x0023, 0x0026 ),
+        ( 0x0028, 0x007E ),
+        ( 0x00A1, 0x00AC ),
+        ( 0x00AE, 0x00FF ),
+        ( 0x0100, 0x017F ),
+        ( 0x0180, 0x024F ),
+        ( 0x2C60, 0x2C7F ),
+        ( 0x16A0, 0x16F0 ),
+        ( 0x0370, 0x0377 ),
+        ( 0x037A, 0x037E ),
+        ( 0x0384, 0x038A ),
+        ( 0x038C, 0x038C ),
+    ]
+
+    alphabet = [
+        chr(code_point) for current_range in include_ranges
+        for code_point in range(current_range[0], current_range[1] + 1)
+    ]
+
+    return ''.join(random.choice(alphabet) for _ in range(length))
+
+
 class SignalDisconnect(object):
     """
     Context manager to disable a signal for a given context. Useful for unit
