8752ebe660ef16a53df1c7f93dc76db09f1b1f03
nickdibari <ndibari@fordham.edu>
2019-02-22 03:29:45 -0500

    Add validation for login redirect param
    Create custom login view

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from accounts import views
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('login/', views.MoodyLoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
     path('update/', views.UpdateInfoView.as_view(), name='update'),
