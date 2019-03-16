e0886baccfca8653351f62969d95d381c3e49c0a
nickdibari <ndibari@fordham.edu>
2019-03-16 16:49:21 -0400

    Build static root directory in standard way

--- mtdj/settings/common.py
+++ mtdj/settings/common.py
@@ -134,7 +134,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '_static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '_static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
