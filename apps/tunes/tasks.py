65062fab47fb5e42dbfb3eb3bd9db27cfebbf02c
nickdibari <ndibari@fordham.edu>
2019-03-23 12:03:51 -0400

    Add retry logic to periodic spotify task

--- apps/tunes/tasks.py
+++ apps/tunes/tasks.py
@@ -7,12 +7,17 @@ from django.core.management import call_command
 logger = get_task_logger(__name__)
 
 
-@task()
-def create_songs_from_spotify_task():
+@task(bind=True, max_retries=3, default_retry_delay=60*15)
+def create_songs_from_spotify_task(self):
     """Periodic task to create songs from Spotify"""
     logger.info('Calling command to create songs from Spotify')
 
     # Disable writing to standard output when running command
     # Everything that we write to stdout gets logged anyway
-    with open(os.devnull, 'w') as dev_null:
-        call_command('tunes_create_songs_from_spotify', stdout=dev_null, stderr=dev_null)
+    try:
+        with open(os.devnull, 'w') as dev_null:
+            call_command('tunes_create_songs_from_spotify', stdout=dev_null, stderr=dev_null)
+
+    except Exception as exc:
+        logger.warning('Exception raised when creating songs from Spotify: {}'.format(exc))
+        self.retry(exc=exc)
