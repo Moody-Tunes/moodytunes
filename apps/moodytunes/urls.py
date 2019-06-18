8c891ca9e4d1bbb8016274fe43f845eea1de0b6c
nickdibari <ndibari@fordham.edu>
2019-06-18 18:56:05 -0400

    First pass at page for suggesting new songs

--- apps/moodytunes/urls.py
+++ apps/moodytunes/urls.py
@@ -5,5 +5,6 @@ from moodytunes import views
 urlpatterns = [
     path('about/', views.AboutView.as_view(), name='about'),
     path('browse/', views.BrowsePlaylistsView.as_view(), name='browse'),
-    path('playlists/', views.EmotionPlaylistsView.as_view(), name='playlists')
+    path('playlists/', views.EmotionPlaylistsView.as_view(), name='playlists'),
+    path('suggest/', views.SuggestSongView.as_view(), name='suggest'),
 ]
--- apps/moodytunes/views.py
+++ apps/moodytunes/views.py
@@ -1,10 +1,19 @@
+import logging
+
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
 
+from accounts.models import UserSuggestedSong
 from base.views import FormView
-from moodytunes.forms import BrowseForm, PlaylistForm
+from moodytunes.forms import BrowseForm, PlaylistForm, SuggestSongForm
 from tunes.utils import CachedPlaylistManager
+
+logger = logging.getLogger(__name__)
 
 
 class AboutView(TemplateView):
@@ -34,3 +43,33 @@ class EmotionPlaylistsView(FormView):
 
     def get_form_instance(self):
         return self.form_class(user=self.request.user)
+
+
+@method_decorator(login_required, name='dispatch')
+class SuggestSongView(FormView):
+    template_name = 'suggest.html'
+    form_class = SuggestSongForm
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            code = form.cleaned_data['code']
+            UserSuggestedSong.objects.create(user=request.user, code=code)
+
+            logger.info(
+                'Saved suggestion for song {} by user {}'.format(code, request.user.username),
+                extra={'fingerprint': 'added_suggested_song'}
+            )
+            messages.info(request, 'Your song has been slated to be added! Keep an eye out for it in the future')
+
+            return HttpResponseRedirect(reverse('moodytunes:suggest'))
+        else:
+            logger.warning(
+                'User {} suggested an invalid song; Errors: {}'.format(
+                    request.user.username,
+                    form.errors
+                ),
+                extra={'fingerprint': 'invalid_suggested_song'}
+            )
+            return render(request, self.template_name, context={'form': form})
