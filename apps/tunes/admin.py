ebbd6894b60386a8cb0695adb1bf59eb4888b438
nickdibari <ndibari@fordham.edu>
2019-08-12 23:52:39 -0400

    Add filter for songs with no genre
    Songs fetched via task will not have genre because of the different workflow

--- apps/tunes/admin.py
+++ apps/tunes/admin.py
@@ -3,6 +3,23 @@ from django.contrib import admin
 from tunes.models import Emotion, Song
 
 
+class NullGenreFilter(admin.SimpleListFilter):
+    title = 'Has Genre'
+    parameter_name = 'has_genre'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('yes', 'Yes'),
+            ('no', 'No')
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'no':
+            return queryset.filter(genre='')
+        if self.value() == 'yes':
+            return queryset.exclude(genre='')
+
+
 class EmotionAdmin(admin.ModelAdmin):
     list_display = ('full_name', 'energy', 'valence')
     readonly_fields = ('name',)
@@ -11,7 +28,7 @@ class EmotionAdmin(admin.ModelAdmin):
 class SongAdmin(admin.ModelAdmin):
     list_display = ('code', 'genre', 'artist', 'name', 'valence', 'energy')
     readonly_fields = ('code', 'artist', 'name', 'valence', 'energy')
-    list_filter = ('genre',)
+    list_filter = (NullGenreFilter, 'genre')
 
 
 admin.site.register(Emotion, EmotionAdmin)
