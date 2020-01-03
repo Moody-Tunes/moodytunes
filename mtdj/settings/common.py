ebd26314a9322cbdb6d68485f3a8a60981e5fa47
nickdibari <ndibari@fordham.edu>
2020-01-02 20:41:58 -0500

    Add settings to enable HSTS

--- mtdj/settings/common.py
+++ mtdj/settings/common.py
@@ -74,6 +74,11 @@ SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
+# Enable HSTS header for site
+SECURE_HSTS_SECONDS = 31536000  # 1 year
+SECURE_HSTS_PRELOAD = True  # Include site in HSTS preload list
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 SESSION_COOKIE_SECURE = True  # Add `secure` flag when setting session cookie
 SESSION_COOKIE_HTTPONLY = True  # Add `HttpOnly` flag when setting session cookie
