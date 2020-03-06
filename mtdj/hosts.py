48f72af5ebdcc1f7527e03ddb54caeb17abfcc5e
nickdibari <ndibari@fordham.edu>
2020-03-06 02:38:56 -0500

    WIP on setting up admin urls

--- mtdj/hosts.py
+++ mtdj/hosts.py
@@ -1,10 +1,9 @@
 from django.conf import settings
-from django.contrib import admin
 from django_hosts import patterns, host
 
 
 host_patterns = patterns(
     '',
-    host(r'admin', admin.site.urls, name='admin'),
+    host(r'admin', 'mtdj.admin_urls', name='admin'),
     host(r'www', settings.ROOT_URLCONF, name='www')
 )
