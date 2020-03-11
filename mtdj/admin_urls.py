b962fcb2da6008d00f93fd24b4dc296139401ab8
nickdibari <ndibari@fordham.edu>
2020-03-11 00:36:36 -0400

    Fix urlpatterns definition for admin urls

--- mtdj/admin_urls.py
+++ mtdj/admin_urls.py
@@ -1,6 +1,15 @@
-from django.conf.urls import include, url
+from django.conf import settings
 from django.contrib import admin
+from django.urls import path, include
+
 
 urlpatterns = [
-    url(r'', include(admin.site.urls, 'admin'), name='admin'),
+    path(r'', admin.site.urls, name='admin'),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
