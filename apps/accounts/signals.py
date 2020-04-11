eb59fd9b35562d08e883755e66d9f0cbbbfc0348
nickdibari <ndibari@fordham.edu>
2020-04-11 00:26:41 -0400

    Only fetch user emotion record if vote is upvoted

--- apps/accounts/signals.py
+++ apps/accounts/signals.py
@@ -22,25 +22,25 @@ post_save.connect(
 )
 
 
-def update_user_attributes(sender, instance, created, *args, **kwargs):
-    # Get the boundary record for the given user and emotion
-    # We should always call this to ensure that if we add new emotions, we'll auto
-    # create the corresponding record when a user starts to browse for songs in
-    # that emotion
-    user_emot, _ = instance.user.useremotion_set.get_or_create(
-        emotion__name=instance.emotion.name,
-        defaults={
-            'user': instance.user,
-            'emotion': instance.emotion
-        }
-    )
-
+def update_user_emotion_attributes(sender, instance, created, *args, **kwargs):
     if instance.vote and created:
-        user_emot.update_attributes()
+        # We should always call get_or_create to ensure that if we add new emotions, we'll auto
+        # create the corresponding UserEmotion record the first time a user votes on a song
+        # for the emotion
+
+        user_emotion, _ = instance.user.useremotion_set.get_or_create(
+            emotion__name=instance.emotion.name,
+            defaults={
+                'user': instance.user,
+                'emotion': instance.emotion
+            }
+        )
+
+        user_emotion.update_attributes()
 
 
 post_save.connect(
-    update_user_attributes,
+    update_user_emotion_attributes,
     sender=UserSongVote,
     dispatch_uid='user_song_vote_post_save_update_useremotion_attributes'
 )
