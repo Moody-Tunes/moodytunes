6a29fe8e0471c8c9ca5f5f0bd8af1c415e99edf7
nickdibari <ndibari@fordham.edu>
2020-04-18 18:36:04 -0400

    Add ordering to emotion playlist response

--- apps/accounts/utils.py
+++ apps/accounts/utils.py
@@ -1,3 +1,6 @@
+from accounts.models import UserSongVote
+
+
 def filter_duplicate_votes_on_song_from_playlist(user_votes):
     """
     Filter queryset of UserSongVotes on unique songs (prevent the same song from appearing twice in the playlist
@@ -7,4 +10,6 @@ def filter_duplicate_votes_on_song_from_playlist(user_votes):
 
     :return: (Queryset) Collection of votes without duplicate votes for the same song
     """
-    return user_votes.distinct('song__code')
+    vote_ids = user_votes.distinct('song__code').values_list('id', flat=True)
+
+    return UserSongVote.objects.select_related('emotion', 'song').filter(id__in=vote_ids).order_by('-created')
