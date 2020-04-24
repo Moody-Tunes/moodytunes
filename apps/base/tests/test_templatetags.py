af8e17e997896a73020c786d06f9db747b386b9a
nickdibari <ndibari@fordham.edu>
2020-04-24 19:53:51 -0400

    Add unit tests for user agent filter

--- apps/base/tests/test_templatetags.py
+++ apps/base/tests/test_templatetags.py
@@ -1,9 +1,11 @@
+from unittest import mock
+
 from django.test import TestCase, override_settings
 
-from base.templatetags.moody_tags import settings
+from base.templatetags.moody_tags import settings, user_agent_is_chrome
 
 
-class TestSettingsTempateTag(TestCase):
+class TestSettingsTemplateTag(TestCase):
     @override_settings(TEST_SETTING='test-setting')
     def test_tag_returns_variable_if_found(self):
         ret = settings('TEST_SETTING')
@@ -17,3 +19,20 @@ class TestSettingsTempateTag(TestCase):
     def test_tag_returns_empty_string_if_variable_not_found(self):
         ret = settings('TEST_SETTING')
         self.assertEqual(ret, '')
+
+
+class TestUserAgentIsChromeTemplateTag(TestCase):
+    def test_tag_returns_true_for_chrome_user_agent(self):
+        request = mock.Mock()
+        request.user_agent.browser.family = 'Chrome'
+        self.assertTrue(user_agent_is_chrome(request))
+
+    def test_tag_returns_true_for_chromium_user_agent(self):
+        request = mock.Mock()
+        request.user_agent.browser.family = 'Chromium'
+        self.assertTrue(user_agent_is_chrome(request))
+
+    def test_tag_returns_false_for_firefox_user_agent(self):
+        request = mock.Mock()
+        request.user_agent.browser.family = 'Firefox'
+        self.assertFalse(user_agent_is_chrome(request))
