42019c19c423cdcd1e058ac1a85dc4f76499c57f
nickdibari <ndibari@fordham.edu>
2020-04-26 00:23:31 -0400

    Set beat to update periodic task last_run after every run

--- mtdj/settings/common_celery.py
+++ mtdj/settings/common_celery.py
@@ -9,6 +9,7 @@ CELERY_TASK_SERIALIZER = 'json'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_RESULT_BACKEND = 'django-db'
+CELERY_BEAT_SYNC_EVERY = 1
 CELERY_RESULT_EXPIRES = timedelta(days=90)  # Delete result records after 90 days
 
 DJANGO_CELERY_RESULTS = {
