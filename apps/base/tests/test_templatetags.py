09445194c1c61456a0d1f3da23270c5057a2e2fb
nickdibari <ndibari@fordham.edu>
2020-05-08 23:55:15 -0400

    Revert "Merge pull request #336 from Moody-Tunes/335_hide_login_through_spotify_on_chrome"
    Got full songs playing on Chrome, so we can display this message on
    Chrome browsers again
    
    This reverts commit 86c135d73c84fa19d3ea1aa2d45fa5259d57110d, reversing
    changes made to 6e54fac42a44c323f82c1d7c3b1829009ca16aac.

--- apps/base/tests/test_templatetags.py
+++ apps/base/tests/test_templatetags.py
@@ -1,11 +1,9 @@
-from unittest import mock
-
 from django.test import TestCase, override_settings
 
-from base.templatetags.moody_tags import settings, user_agent_is_chrome
+from base.templatetags.moody_tags import settings
 
 
-class TestSettingsTemplateTag(TestCase):
+class TestSettingsTempateTag(TestCase):
     @override_settings(TEST_SETTING='test-setting')
     def test_tag_returns_variable_if_found(self):
         ret = settings('TEST_SETTING')
@@ -19,20 +17,3 @@ class TestSettingsTemplateTag(TestCase):
     def test_tag_returns_empty_string_if_variable_not_found(self):
         ret = settings('TEST_SETTING')
         self.assertEqual(ret, '')
-
-
-class TestUserAgentIsChromeTemplateTag(TestCase):
-    def test_tag_returns_true_for_chrome_user_agent(self):
-        request = mock.Mock()
-        request.user_agent.browser.family = 'Chrome'
-        self.assertTrue(user_agent_is_chrome(request))
-
-    def test_tag_returns_true_for_chromium_user_agent(self):
-        request = mock.Mock()
-        request.user_agent.browser.family = 'Chromium'
-        self.assertTrue(user_agent_is_chrome(request))
-
-    def test_tag_returns_false_for_firefox_user_agent(self):
-        request = mock.Mock()
-        request.user_agent.browser.family = 'Firefox'
-        self.assertFalse(user_agent_is_chrome(request))
