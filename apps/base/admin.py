ea2b2c983feb0e39009b53f693ec6be2a56b6aaf
nickdibari <ndibari@fordham.edu>
2020-05-16 00:27:53 -0400

    Add primary key to MoodyBaseAdmin list_display

--- apps/base/admin.py
+++ apps/base/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 
 class MoodyBaseAdmin(admin.ModelAdmin):
-    list_display = ('created', 'updated')
+    list_display = ('created', 'updated', 'pk')
 
     def get_list_display(self, request):
         return self.list_display + MoodyBaseAdmin.list_display
