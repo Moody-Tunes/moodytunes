619aa5af780983d858e641fb2361218644bba907
nickdibari <ndibari@fordham.edu>
2020-05-16 00:33:56 -0400

    Refactor primary key to first position in list_display

--- apps/base/admin.py
+++ apps/base/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 
 class MoodyBaseAdmin(admin.ModelAdmin):
-    list_display = ('created', 'updated', 'pk')
+    list_display = ('created', 'updated')
+    primary_key_value = ('pk',)
 
     def get_list_display(self, request):
-        return self.list_display + MoodyBaseAdmin.list_display
+        return self.primary_key_value + self.list_display + MoodyBaseAdmin.list_display
