618744c72d4e4fca48daf40476b49020e1b79221
nickdibari <ndibari@fordham.edu>
2020-05-16 21:58:00 -0400

    Add periodic task to dump data from mission critical database tables

--- apps/base/tasks.py
+++ apps/base/tasks.py
@@ -2,7 +2,10 @@ from logging import getLogger
 
 from celery.schedules import crontab
 from celery.task import PeriodicTask, Task
+from django.conf import settings
 from django.core.management import call_command
+
+from libs.moody_logging import auto_fingerprint, update_logging_data
 
 
 logger = getLogger(__name__)
@@ -26,3 +29,30 @@ class ClearExpiredSessionsTask(MoodyPeriodicTask):
         logger.info('Calling django management command to clear expired sessions')
 
         call_command('clearsessions')
+
+
+class BackupDatabaseTask(MoodyPeriodicTask):
+    run_every = crontab(minute=0, hour=3, day_of_week=0)
+
+    """Task to backup mission critical database tables"""
+    @update_logging_data
+    def run(self, *args, **kwargs):
+        logger.info(
+            'Starting run to backup mission critical database tables',
+            extra={'fingerprint': auto_fingerprint('start_database_backup', **kwargs)}
+        )
+
+        for model in settings.DATABASE_BACKUP_TARGETS:
+            backup_filename = '{backup_directory}/{model_name}.json'.format(
+                backup_directory=settings.DATABASE_BACKUPS_PATH,
+                model_name=model
+            )
+
+            logger.info('Writing backup of {} to file {}'.format(model, backup_filename))
+
+            call_command('dumpdata', model, output=backup_filename)
+
+        logger.info(
+            'Finished run to backup mission critical database tables',
+            extra={'fingerprint': auto_fingerprint('finished_database_backup', **kwargs)}
+        )
