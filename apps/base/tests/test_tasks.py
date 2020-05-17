d15bd7c0ccf9ae6acea87a4a10563df195637402
nickdibari <ndibari@fordham.edu>
2020-05-16 22:20:37 -0400

    Add unit tests for BackupDatabaseTask

--- apps/base/tests/test_tasks.py
+++ apps/base/tests/test_tasks.py
@@ -1,3 +1,4 @@
+import os
 from datetime import timedelta
 
 from django.conf import settings
@@ -5,7 +6,7 @@ from django.contrib.sessions.models import Session
 from django.test import TestCase
 from django.utils import timezone
 
-from base.tasks import ClearExpiredSessionsTask
+from base.tasks import BackupDatabaseTask, ClearExpiredSessionsTask
 
 
 class TestClearExpiredSessionsTask(TestCase):
@@ -19,3 +20,20 @@ class TestClearExpiredSessionsTask(TestCase):
 
         ClearExpiredSessionsTask().run()
         self.assertEqual(Session.objects.count(), 0)
+
+
+class TestBackupDatabaseTask(TestCase):
+    def test_happy_path(self):
+        BackupDatabaseTask().run()
+
+        for model in settings.DATABASE_BACKUP_TARGETS:
+            backup_filename = os.path.join(settings.DATABASE_BACKUPS_PATH, f'{model}.json')
+            self.assertTrue(os.path.exists(backup_filename))
+
+    def test_delete_old_backups_clears_files(self):
+        with open(os.path.join(settings.DATABASE_BACKUPS_PATH, 'test'), 'w') as test_file:
+            test_file.write('Hello World!')
+
+        BackupDatabaseTask().delete_old_backups()
+
+        self.assertFalse(os.listdir(settings.DATABASE_BACKUPS_PATH))
