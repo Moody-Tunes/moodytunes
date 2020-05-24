a97cf48b5ed8f511e61ea8fe9567e1317cc2f569
nickdibari <ndibari@fordham.edu>
2020-05-23 21:50:23 -0400

    Add log to start of export playlist task

--- apps/moodytunes/tasks.py
+++ apps/moodytunes/tasks.py
@@ -184,9 +184,19 @@ class CreateSpotifyPlaylistFromSongsTask(MoodyBaseTask):
 
         return auth
 
+    @update_logging_data
     def run(self, auth_id, playlist_name, songs, *args, **kwargs):
         auth = self.get_and_refresh_spotify_user_auth_record(auth_id)
         spotify = SpotifyClient(identifier='create_spotify_playlist_from_songs_{}'.format(auth.spotify_user_id))
+
+        logger.info(
+            'Exporting songs to playlist {} for user {} on Spotify'.format(playlist_name, auth.user.username),
+            extra={
+                'fingerprint': auto_fingerprint('start_export_playlist', **kwargs),
+                'songs': songs,
+                'auth_id': auth.pk
+            }
+        )
 
         playlist_id = self.get_or_create_playlist(auth.access_token, auth.spotify_user_id, playlist_name, spotify)
         self.add_songs_to_playlist(auth.access_token, playlist_id, songs, spotify)
