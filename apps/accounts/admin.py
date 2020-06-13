de43afc000c73ada7c7c73564d0892c7074e48e1
nickdibari <ndibari@fordham.edu>
2020-06-12 22:39:05 -0400

    Add intentional pre-commit error

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -69,6 +69,7 @@ class UserSongVoteAdmin(MoodyBaseAdmin):
     search_fields = ('user__username',)
 
     def has_add_permission(self, request):
+        import pdb; pdb.set_trace()
         return False
 
 
