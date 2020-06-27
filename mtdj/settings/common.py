bb6effae40389bc0afb9793f4ebe10bf7864f6f7
nickdibari <ndibari@fordham.edu>
2020-06-26 20:13:25 -0400

    Add custom admin base template for admin site
    Add link to favion image from static files

--- mtdj/settings/common.py
+++ mtdj/settings/common.py
@@ -100,7 +100,9 @@ DEFAULT_HOST = 'www'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
