67169cfe7d8545b8c01f0aa8057449646f22a953
nickdibari <ndibari@fordham.edu>
2020-07-03 22:01:21 -0400

    Fix short circuit middleware to work with Django 2

--- apps/base/middleware.py
+++ apps/base/middleware.py
@@ -1,3 +1,6 @@
+from django.utils.deprecation import MiddlewareMixin
+
+
 def shortcircuitmiddleware(f):
     """View decorator, the sole purpose to is 'rename' the function'_shortcircuitmiddleware' """
     def _shortcircuitmiddleware(*args, **kwargs):
@@ -5,7 +8,7 @@ def shortcircuitmiddleware(f):
     return _shortcircuitmiddleware
 
 
-class ShortCircuitMiddleware(object):
+class ShortCircuitMiddleware(MiddlewareMixin):
     """ Middleware; looks for a view function named '_shortcircuitmiddleware'
     and short-circuits. Relies on the fact that if you return an HttpResponse
     from a view, it will short-circuit other middleware, see:
@@ -13,6 +16,6 @@ class ShortCircuitMiddleware(object):
      """
 
     def process_view(self, request, view_func, view_args, view_kwargs):
-        if view_func.func_name == shortcircuitmiddleware.__name__:
+        if view_func.__name__ == shortcircuitmiddleware.__name__:
             return view_func(request, *view_args, **view_kwargs)
         return None
