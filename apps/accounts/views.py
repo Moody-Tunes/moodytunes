b918be56726ee589d0e6e05bd3f8f6ce20af5c36
nickdibari <ndibari@fordham.edu>
2020-07-31 19:09:26 -0400

    Govern param to show Spotify auth prompt behind switch

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,5 +1,6 @@
 import logging
 
+import waffle
 from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -45,18 +46,20 @@ class MoodyLoginView(LoginView):
         redirect_url = super().get_redirect_url()
 
         if not redirect_url:
-            show_spotify_auth = True
+            show_spotify_auth = False
 
-            # Check if user has authenticated with Spotify, to prompt user to
-            # authenticate if they have not already done so
-            if self.request.user.is_authenticated:
-                show_spotify_auth = not SpotifyUserAuth.objects.filter(user=self.request.user).exists()
+            if waffle.switch_is_active('show_spotify_auth_prompt'):
 
-                # Check if user has explicitly indicated they do not want to
-                # authenticate with Spotify
-                if show_spotify_auth and hasattr(self.request.user, 'userprofile'):
-                    user_profile = self.request.user.userprofile
-                    show_spotify_auth = not user_profile.has_rejected_spotify_auth
+                # Check if user has authenticated with Spotify, to prompt user to
+                # authenticate if they have not already done so
+                if self.request.user.is_authenticated:
+                    show_spotify_auth = not SpotifyUserAuth.objects.filter(user=self.request.user).exists()
+
+                    # Check if user has explicitly indicated they do not want to
+                    # authenticate with Spotify
+                    if show_spotify_auth and hasattr(self.request.user, 'userprofile'):
+                        user_profile = self.request.user.userprofile
+                        show_spotify_auth = not user_profile.has_rejected_spotify_auth
 
             return f'{settings.LOGIN_REDIRECT_URL}?show_spotify_auth={show_spotify_auth}'
 
