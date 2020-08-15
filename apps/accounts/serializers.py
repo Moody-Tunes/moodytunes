3c7bf384dcaade6b88dd920e4e9b1959f2a73a5b
nickdibari <ndibari@fordham.edu>
2020-08-15 00:15:19 -0400

    Delete AnalyticsView

--- apps/accounts/serializers.py
+++ apps/accounts/serializers.py
@@ -1,22 +1,6 @@
 from rest_framework import serializers
 
-from accounts.models import UserProfile, UserSongVote
-from tunes.models import Emotion
-
-
-class AnalyticsSerializer(serializers.Serializer):
-    emotion_name = serializers.CharField(max_length=20)
-    energy = serializers.FloatField(min_value=0, max_value=1)
-    valence = serializers.FloatField(min_value=0, max_value=1)
-    danceability = serializers.FloatField(min_value=0, max_value=1)
-    total_songs = serializers.IntegerField()
-
-
-class AnalyticsRequestSerializer(serializers.Serializer):
-    genre = serializers.CharField(max_length=15, required=False)
-    emotion = serializers.ChoiceField(choices=Emotion.EMOTION_NAME_CHOICES)
-    context = serializers.ChoiceField(choices=UserSongVote.CONTEXT_CHOICES, required=False)
-    artist = serializers.CharField(max_length=50, required=False)
+from accounts.models import UserProfile
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
