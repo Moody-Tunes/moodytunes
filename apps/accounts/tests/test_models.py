72f0f4e82c9665946bb2b07d50435ce809eb821a
nickdibari <ndibari@fordham.edu>
2020-08-21 22:44:36 -0400

    Remove SpotifyClient from MoodyTunes codebase
    Cleanup lingering references to MoodyTunes Spotify client

--- apps/accounts/tests/test_models.py
+++ apps/accounts/tests/test_models.py
@@ -6,10 +6,10 @@ from django.core.exceptions import ValidationError
 from django.db.models.signals import post_save
 from django.test import TestCase
 from django.utils import timezone
+from spotify_client.exceptions import SpotifyException
 
 from accounts.models import MoodyUser, SpotifyUserAuth, SpotifyUserData, UserEmotion, UserSongVote
 from accounts.signals import create_user_emotion_records, update_user_emotion_attributes
-from libs.spotify import SpotifyException
 from libs.tests.helpers import MoodyUtil, SignalDisconnect
 from libs.utils import average
 from tunes.models import Emotion
