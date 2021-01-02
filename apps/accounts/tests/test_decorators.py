0f34e6e6362fc98891975ccb33d47bb078a48688
nickdibari <ndibari@fordham.edu>
2021-01-02 00:28:42 -0500

    Use functions for dummy views in test spotify_auth_required
    Add test to ensure SpotifyUserAuth value is cached in request

--- apps/accounts/tests/test_decorators.py
+++ apps/accounts/tests/test_decorators.py
@@ -1,7 +1,5 @@
 from django.http.response import Http404, HttpResponse
 from django.test import RequestFactory, TestCase
-from django.utils.decorators import method_decorator
-from django.views.generic import View
 from rest_framework import status
 
 from accounts.decorators import spotify_auth_required
@@ -11,16 +9,14 @@ from libs.tests.helpers import MoodyUtil
 REDIRECT_URI = '/foo/'
 
 
-@method_decorator(spotify_auth_required(REDIRECT_URI),  name='dispatch')
-class DummySpotifyAuthRequiredView(View):
-    def get(self, request, *args, **kwargs):
-        return HttpResponse()
+@spotify_auth_required(REDIRECT_URI)
+def dummy_spotify_auth_required_view(request):
+    return HttpResponse()
 
 
-@method_decorator(spotify_auth_required(REDIRECT_URI, raise_exc=True),  name='dispatch')
-class DummySpotifyAuthRequiredViewWithExcRaise(View):
-    def get(self, request, *args, **kwargs):
-        return HttpResponse()
+@spotify_auth_required(REDIRECT_URI, raise_exc=True)
+def dummy_spotify_auth_required_raise_exc_view(request):
+    return HttpResponse()
 
 
 class TestSpotifyAuthRequiredDecorator(TestCase):
@@ -36,15 +32,16 @@ class TestSpotifyAuthRequiredDecorator(TestCase):
         request = self.factory.get('/bar/')
         request.user = self.user_with_auth
 
-        resp = DummySpotifyAuthRequiredView.as_view()(request)
+        resp = dummy_spotify_auth_required_view(request)
 
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        self.assertEqual(request.spotify_auth.id, self.spotify_auth.id)  # Ensure decorator caches auth on request
 
     def test_decorator_redirects_to_supplied_uri_for_user_without_spotify_auth(self):
         request = self.factory.get('/bar/')
         request.user = self.user_without_auth
 
-        resp = DummySpotifyAuthRequiredView.as_view()(request)
+        resp = dummy_spotify_auth_required_view(request)
 
         self.assertEqual(resp.status_code, status.HTTP_302_FOUND)
         self.assertEqual(resp['Location'], REDIRECT_URI)
@@ -54,4 +51,4 @@ class TestSpotifyAuthRequiredDecorator(TestCase):
         request.user = self.user_without_auth
 
         with self.assertRaises(Http404):
-            DummySpotifyAuthRequiredViewWithExcRaise.as_view()(request)
+            dummy_spotify_auth_required_raise_exc_view(request)
