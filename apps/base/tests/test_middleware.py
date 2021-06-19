29b9361f8113b8445c002dbda6764885cf4f3001
nickdibari <ndibari@fordham.edu>
2021-06-19 18:49:32 -0400

    Add unit test for trace_id set through request header

--- apps/base/tests/test_middleware.py
+++ apps/base/tests/test_middleware.py
@@ -8,3 +8,10 @@ class TestAddTraceIdToRequestMiddleware(TestCase):
         response = self.client.get(url)
 
         self.assertTrue(hasattr(response.wsgi_request, 'trace_id'))
+
+    def test_request_with_trace_header_sets_request_trace_id_to_header_value(self):
+        trace_id = 'test-trace-id'
+        url = reverse('accounts:login')
+        response = self.client.get(url, HTTP_X_TRACE_ID=trace_id)
+
+        self.assertEqual(response.wsgi_request.trace_id, trace_id)
