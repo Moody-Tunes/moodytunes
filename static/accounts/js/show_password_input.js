a73ca607f7e5adfd1bfaabea8d3aed7016b35640
nickdibari <ndibari@fordham.edu>
2021-07-02 21:18:05 -0400

    Add show password logic for account creation page

--- static/accounts/js/show_password_input.js
+++ static/accounts/js/show_password_input.js
@@ -1,13 +1,20 @@
 'use strict';
 
 (function IIFE() {
-    let checkbox = document.getElementById('show-password');
+    let showPasswordInput = document.getElementById('show-password');
     let passwordInput = document.getElementById('id_password');
     let confirmPasswordInput = document.getElementById('id_confirm_password');
 
-    checkbox.addEventListener('click', function () {
+    showPasswordInput.addEventListener('click', function () {
         const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
         passwordInput.setAttribute('type', type);
         confirmPasswordInput.setAttribute('type', type);
+
+        let showPasswordImage = showPasswordInput.dataset.showPasswordImg;
+        if (type === 'text') {
+            showPasswordImage = showPasswordInput.dataset.hidePasswordImg;
+        }
+
+        showPasswordInput.setAttribute('src', showPasswordImage);
     })
 })();
